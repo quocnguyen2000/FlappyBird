e018c164549e19a201551d0fce1231ad69250b7c
Shun <quocnguyent10v>
2020-05-15 10:56:14 +0700

    FlappyBird

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public float scrollSpeed = -1.5f;
+	public bool gameOver = false;
+	public Text scoreText;
+	public static GameController instance;
+	public GameObject gameOverText;
+
+	private int score = 0;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject); 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameOver == true && Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
+
+	public void BirdScored () {
+		if (gameOver) {
+			return;
+		}
+		score++;
+		scoreText.text = "Score: " + score.ToString ();
+	}
+
+	public void BirdDied () {
+		gameOverText.SetActive (true);
+		gameOver = true;
+	}
+}
